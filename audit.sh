af9e748b87b94839246687d8db8917d66d7ea606
suffereddruid7 <foreverhungrym8@gmail.com>
2017-07-16 15:27:43 +0800

    Formatting

--- audit.sh
+++ audit.sh
@@ -11,68 +11,70 @@ echo "##########################################################"
 
 echo "##########################################################"
 # File System Configuration
+echo "File System Configuration"
 
 
 echo "##########################################################"
 # 1.14 Set Sticky Bit on All World-Writable Directories
-echo "df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null"
+echo "1.14 Set Sticky Bit on All World-Writable Directories"
+echo "$ df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null"
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null
 
 echo "##########################################################"
 # 1.15 Disable Mounting of Legacy Filesystem
 # cramfs Filesystem
 echo "cramfs Filesystem"
-echo "/sbin/modprobe -n -v cramfs"
+echo "$ /sbin/modprobe -n -v cramfs"
 /sbin/modprobe -n -v cramfs
-echo "/sbin/lsmod | grep cramfs"
+echo "$ /sbin/lsmod | grep cramfs"
 /sbin/lsmod | grep cramfs
 
 echo "##########################################################"
 # freevxfs Filesystem
 echo "freevxfs Filesystem"
-echo "/sbin/modprobe -n -v freevxfs"
+echo "$ /sbin/modprobe -n -v freevxfs"
 /sbin/modprobe -n -v freevxfs
-echo "/sbin/lsmod | grep freevxfs"
+echo "$ /sbin/lsmod | grep freevxfs"
 /sbin/lsmod | grep freevxfs
 
 echo "##########################################################"
 # jffs2 Filesystem
 echo "jffs2 Filesystem"
-echo "/sbin/modprobe -n -v jffs2"
+echo "$ /sbin/modprobe -n -v jffs2"
 /sbin/modprobe -n -v jffs2
-echo "/sbin/lsmod | grep jffs2"
+echo "$ /sbin/lsmod | grep jffs2"
 /sbin/lsmod | grep jffs2
 
 echo "##########################################################"
 # hfs Filesystem
 echo "hfs Filesystem"
-echo "/sbin/modprobe -n -v hfs"
+echo "$ /sbin/modprobe -n -v hfs"
 /sbin/modprobe -n -v hfs
-echo "/sbin/lsmod | grep hfs"
+echo "$ /sbin/lsmod | grep hfs"
 /sbin/lsmod | grep hfs
 
 echo "##########################################################"
 # hfsplus Filesystem
 echo "hfsplus Filesystem"
-echo "/sbin/modprobe -n -v hfsplus"
+echo "$ /sbin/modprobe -n -v hfsplus"
 /sbin/modprobe -n -v hfsplus
-echo "/sbin/lsmod | grep hfsplus"
+echo "$ /sbin/lsmod | grep hfsplus"
 /sbin/lsmod | grep hfsplus
 
 echo "##########################################################"
 # squashfs Filesystem
 echo "squashfs Filesystem"
-echo "/sbin/modprobe -n -v squashfs"
+echo "$ /sbin/modprobe -n -v squashfs"
 /sbin/modprobe -n -v squashfs
-echo "/sbin/lsmod | grep squashfs"
+echo "$ /sbin/lsmod | grep squashfs"
 /sbin/lsmod | grep squashfs
 
 echo "##########################################################"
 # udf Filesystem
 echo "udf Filesystem"
-echo "/sbin/modprobe -n -v udf"
+echo "$ /sbin/modprobe -n -v udf"
 /sbin/modprobe -n -v udf
-echo "/sbin/lsmod | grep udf"
+echo "$ /sbin/lsmod | grep udf"
 /sbin/lsmod | grep udf
 
 echo "##########################################################"
--- remedy.sh
+++ remedy.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 # Remedy Script for RHEL 7 based on CIS BenchMarks
+# Script misc. section
 trap '' 2 20
-echo "##########################################################"
-echo "Remedy Script"
-echo "RHEL 7"
-echo "##########################################################"
-
 datetime=`date +"%m%d%y-%H%M"`
 exec > >(tee "/root/remedy-"$datetime".txt") 2>&1
+echo "##########################################################"
+echo "FIA Remedy Script"
+echo "Red Hat Enterprise Linux 7"
+echo "##########################################################"
 
+echo "##########################################################"
+# File System Configuration
 echo "File System Configuration"
 
-
+echo "##########################################################"
+# 1.14 Set Sticky Bit on All World-Writable Directories
 echo "1.14 Set Sticky Bit on All World-Writable Directories"
 echo "$ df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t"
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
 
+echo "##########################################################"
+# 1.15 Disable Mounting of Legacy Filesystems"
 echo "1.15 Disable Mounting of Legacy Filesystems"
 echo "$ touch /etc/modprobe.d/CIS.conf"
 touch /etc/modprobe.d/CIS.conf
