9b2bc09a152d99e12431d894921c019caef4941c
suffereddruid7 <foreverhungrym8@gmail.com>
2017-07-21 01:50:14 +0800

    Legacy Services

--- remedy.sh
+++ remedy.sh
@@ -12,22 +12,14 @@ exec > >(tee "/root/remedy-"$datetime".txt") 2>&1
 echo "##########################################################"
 echo "FIA Remedy Script"
 echo "Red Hat Enterprise Linux 7"
-echo "##########################################################"
 
 echo "##########################################################"
-# File System Configuration
 echo "File System Configuration"
 
-echo "##########################################################"
-# 1.14 Set Sticky Bit on All World-Writable Directories
-echo "1.14 Set Sticky Bit on All World-Writable Directories"
-echo "$ df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t"
+echo "Set Sticky Bit on All World-Writable Directories"
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
 
-echo "##########################################################"
-# 1.15 Disable Mounting of Legacy Filesystems"
-echo "1.15 Disable Mounting of Legacy Filesystems"
-echo "$ touch /etc/modprobe.d/CIS.conf"
+echo "Disable Mounting of Legacy Filesystems"
 touch /etc/modprobe.d/CIS.conf
 /bin/cat << EOM > /etc/modprobe.d/CIS.conf
 install cramfs /bin/true
@@ -39,19 +31,25 @@ install squashfs /bin/true
 install udf /bin/true
 EOM
 
-echo "##########################################################"
-# Patching the Linux System
 echo "Patching the Linux System"
-echo "$ cat /etc/redhat-release"
 cat /etc/redhat-release
 
-echo "##########################################################"
-# 2. Remove Legacy Services
-echo "2. Remove Legacy Service"
-# 2.1 Remove telnet clients & server
-echo "$ yum -y erase telnet-server"
+echo "Remove Legacy Service"
 yum -y erase telnet-server
-echo "$ yum -y erase telnet"
 yum -y erase telnet
+yum -y erase rsh-server	
+yum -y erase rsh
+yum -y erase ypbind	
+yum -y erase ypserv	
+yum -y erase tftp
+yum -y erase tftp-server
+yum -y erase xinetd
+chkconfig chargen-dgram off
+chkconfig chargen-stream off	
+chkconfig daytime-dgram off
+chkconfig daytime-stream off	
+chkconfig echo-dgram off	
+chkconfig echo-stream off	
+chkconfig tcpmux-server off
 
-echo "##########################################################"
+echo "Special Services"
