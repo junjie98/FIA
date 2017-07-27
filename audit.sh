2c5ffcc460d5a8aa7d43f6448d8f8c47cca88e11
suffereddruid7 <foreverhungrym8@gmail.com>
2017-07-27 15:04:35 +0800

    4.2 Set Permissions on /boot/grub2/grub.cfg

--- audit.sh
+++ audit.sh
@@ -161,3 +161,23 @@ else
 	echo "$count. /boot/grub2/grub.cfg - FAILED (Owner and group owner of file is configured incorrectly)"
 	((count++))
 fi 
+
+# 4.2 Set Permissions on /boot/grub2/grub.cfg
+checkbootloaderuser=`grep "^set superusers" /boot/grub2/grub.cfg`
+
+if [ -z "$checkbootloaderuser" ]
+then
+	echo "$count. Boot Loader Password - FAILED (Boot loader is not configured with any superuser)"
+	((count++))
+else
+	checkbootloaderpassword=`grep "^password" /boot/grub2/grub.cfg`
+
+	if [ -z "$checkbootloaderpassword" ]
+	then
+		echo "$count. Boot Loader Password - FAILED (Boot loader is not configured with a password)"
+		((count++))
+	else
+		echo "$count. Boot Loader Password - PASSED (Boot loader is configured with a superuser and password)"
+		((count++))
+	fi
+fi
