9ede4267a8b2ab78492dbddf4ff7d10c11ea00fd
1505888F <1505888F@student.tp.edu.sg>
2017-07-30 01:33:14 +0800

    1.1 to 6.2.1.10

--- audit.sh
+++ audit.sh
@@ -1,11 +1,310 @@
 #!/bin/bash
 # Audit Script for RHEL 7 based on CIS BenchMarks
 # Script misc. section
+
 trap '' 2 20
+trap '' SIGTSTP
+
 # Check if script is executed by root
 if [ "$EUID" -ne 0 ]
 	then echo "Please run this script as root"
 	exit
+fi
+
+# 1.1 Create seperate partition for /tmp
+checktmp=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab`
+
+if [ -z "$checktmp" ]
+then
+	echo "1. /tmp - FAILED (A separate /tmp partition has not been created.)"
+else
+	checknodev=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab | grep nodev` # 1.2 Set nodev option for partition
+	checknodev1=`mount | grep "[[:space:]]/tmp[[:space:]]" | grep nodev`  
+	if [ -z "$checknodev" -a -z "$checknodev1" ]
+	then
+		echo "1. /tmp - FAILED (/tmp not mounted with nodev option)"
+	elif [ -z "$checknodev" -a -n "$checknodev1" ]
+	then
+		echo "1. /tmp - FAILED (/tmp not mounted persistently with nodev option)"
+	elif [ -n "$checknodev" -a -z "$checknodev1" ]
+	then
+		echo "1. /tmp - FAILED (/tmp currently not mounted with nodev option)"
+	else
+		checknosuid=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab | grep nosuid` # 1.3 Set nosuid option for partition
+		checknosuid1=`mount | grep "[[:space:]]/tmp[[:space:]]" | grep nosuid`
+		if [ -z "$checknosuid" -a -z "$checknosuid1" ]
+		then
+			echo "1. /tmp - FAILED (/tmp not mounted with nosuid option)"
+		elif [ -z "$checknosuid" -a -n "$checknosuid1" ]
+		then
+			echo "1. /tmp - FAILED (/tmp not mounted persistently with nosuid option)"
+		elif [ -n "$checknosuid" -a -z "$checknosuid1" ]
+		then
+			echo "1. /tmp - FAILED (/tmp currently not mounted with nosuid option)"
+		else	
+			checknoexec=`grep "[[:space:]]/tmp[[:space:]]" /etc/fstab | grep noexec` # 1.4 Set noexec option for partition
+			checknoexec1=`mount | grep "[[:space:]]/tmp[[:space:]]" | grep noexec`
+			if [ -z "$checknoexec" -a -z "$checknoexec1" ]
+			then
+				echo "1. /tmp - FAILED (/tmp not mounted with noexec option)"
+			elif [ -z "$checknoexec" -a -n "$checknoexec1" ]
+			then
+				echo "1. /tmp - FAILED (/tmp not mounted persistently with noexec option)"
+			elif [ -n "$checknoexec" -a -z "$checknoexec1" ]
+			then
+				echo "1. /tmp - FAILED (/tmp currently not mounted with noexec option)"
+			else
+				echo "1. /tmp - PASSED (/tmp is a separate partition with nodev,nosuid,noexec option)"
+			fi
+		fi
+	fi
+fi
+ 
+# 1.5 Create seperate partition for /var
+checkvar=`grep "[[:space:]]/var[[:space:]]" /etc/fstab`
+if [ -z "$checkvar" ]
+then
+	echo "2. /var - FAILED (A separate /var partition has not been created.)"
+else 
+	echo "2. /var - PASSED (A separate /var partition has been created)"
+fi	
+
+# 1.6 Bind mount /var/tmp directory to /tmp
+checkbind=`grep -e "^/tmp[[:space:]]" /etc/fstab | grep /var/tmp` 
+checkbind1=`mount | grep /var/tmp`
+if [ -z "$checkbind" -a -z "$checkbind1" ]
+then
+	echo "3. /var/tmp - FAILED (/var/tmp mount is not bounded to /tmp)"
+elif [ -z "$checkbind" -a -n "$checkbind1" ]
+then
+	echo "3. /var/tmp - FAILED (/var/tmp mount has not been binded to /tmp persistently.)"
+elif [ -n "$checkbind" -a -z "$checkbind1" ]
+then
+	echo "3. /var/tmp - FAILED (/var/tmp mount is not currently bounded to /tmp)"
+else 
+	echo "3. /var/tmp - PASSED (/var/tmp has been binded and mounted to /tmp)"
+fi
+
+# 1.7 Set nosuid option for partition
+checkvarlog=`grep "[[:space:]]/var/log[[:space:]]" /etc/fstab`
+if [ -z "$checkvarlog" ]
+then
+	echo "4. /var/log - FAILED (A separate /var/log partition has not been created.)"
+else 
+	echo "4. /var/log - PASSED (A separate /var/log partition has been created)"
+fi	
+
+# 1.8 Create seperate partition for /var/log/audit
+checkvarlogaudit=`grep "[[:space:]]/var/log/audit[[:space:]]" /etc/fstab`
+if [ -z "$checkvarlogaudit" ]
+then
+	echo "5. /var/log/audit - FAILED (A separate /var/log/audit partition has not been created.)"
+else 
+	echo "5. /var/log/audit - PASSED (A separate /var/log/audit partition has been created)"
+fi	
+
+# 1.9 Create seperate partition for /home
+checkhome=` grep "[[:space:]]/home[[:space:]]" /etc/fstab`
+if [ -z "$checkhome" ]
+then
+	echo "6. /home - FAILED (A separate /home partition has not been created.)"
+else 
+	 checknodevhome=`grep "[[:space:]]/home[[:space:]]" /etc/fstab | grep nodev` # 1.10 Add nodev option to /home
+	 checknodevhome1=`mount | grep "[[:space:]]/home[[:space:]]" | grep nodev`
+	
+		if [ -z "$checknodevhome" -a -z "$checknodevhome1" ]
+		then
+			echo "6. /home - FAILED (/home not mounted with nodev option)"
+		elif [ -z "$checknodevhome" -a -n "$checknodevhome1" ]
+		then
+			echo "6. /home - FAILED (/home not mounted persistently with nodev option)"
+		elif [ -n "$checknodevhome" -a -z "$checknodevhome1" ]
+		then
+			echo "6. /home - FAILED (/home currently not mounted with nodev option)"
+	else
+		echo "6. /home - PASSED (/home is a separate partition with nodev option)"
+	fi
+fi
+
+# 1.11 Add nodev option to removable media partitions
+cdcheck=`grep cd /etc/fstab` 
+if [ -n "$cdcheck" ]
+then
+	cdnodevcheck=`grep cdrom /etc/fstab | grep nodev` # 1.12 Add noexec option to removable media partitions
+	cdnosuidcheck=`grep cdrom /etc/fstab | grep nosuid` # 1.13 Add nosuid option to removable media partitions
+	cdnosuidcheck=`grep cdrom /etc/fstab | grep noexec` # 1.14 
+	if [ -z "$cdnosuidcheck" ]
+	then
+			echo "7. /cdrom - FAILED (/cdrom not mounted with nodev option)"
+	elif [ -z "$cdnosuidcheck" ]
+	then
+			echo "7. /cdrom - FAILED (/cdrom not mounted with nosuid option)"
+	elif [ -z "$cdnosuidcheck" ]
+	then
+			echo "7. /cdrom - FAILED (/cdrom not mounted with noexec option)"
+	else
+		"7. /cdrom - PASSED (/cdrom is a mounted with nodev,nosuid,noexec option)"
+	fi
+else
+	echo "7. /cdrom - PASSED (/cdrom not mounted)"
+fi
+
+# 1.14 Set sticky bit on all world-writable directories
+checkstickybit=`df --local -P | awk {'if (NR1=1) print $6'} | xargs -l '{}' -xdev -type d \(--perm -0002 -a ! -perm -1000 \) 2> /dev/null`
+if [ -n "$checkstickybit" ]
+then
+	echo "8. Sticky Bit - FAILED (Sticky bit is not set on all world-writable directories)"
+else
+	echo "8. Sticky Bit - PASSED (Sticky bit is set on all world-writable directories)"
+fi
+
+# 1.15 Disable mounting of legacy filesystems
+checkcramfs=`/sbin/lsmod | grep cramfs` 
+checkfreevxfs=`/sbin/lsmod | grep freevxfs`
+checkjffs2=`/sbin/lsmod | grep jffs2`
+checkhfs=`/sbin/lsmod | grep hfs`
+checkhfsplus=`/sbin/lsmod | grep hfsplus`
+checksquashfs=`/sbin/lsmod | grep squashfs`
+checkudf=`/sbin/lsmod | grep udf`
+
+if [ -n "$checkcramfs" -o -n "$checkfreevxfs" -o -n "$checkjffs2" -o -n "$checkhfs" -o -n "$checkhfsplus" -o -n "$checksquashfs" -o -n "$checkudf" ]
+then
+	echo "8. Legacy File Systems - FAILED (Not all legacy file systems are disabled i.e. cramfs, freevxfs, jffs2, hfs, hfsplus, squashfs and udf)"
+else
+	echo "8. Legacy File Systems - PASSED (All legacy file systems are disabled i.e. cramfs, freevxfs, jffs2, hfs, hfsplus, squashfs and udf)"
+fi
+
+printf "\n"
+printf "Services\n"
+
+# 2.1-2.10 Checking of services
+services=( "telnet" "telnet-server" "rsh-server" "rsh" "ypserv" "ypbind" "tftp" "tftp-server" "xinetd" )
+
+count=1
+for eachservice in ${services[*]}
+do 
+	yum -q list installed $eachservice &>/dev/null && echo "$count. $eachservice - FAILED ($eachservice is Installed)" || echo "$count. $eachservice - PASSED ($eachservice is not installed) "
+	((count++))
+done 	
+
+
+chkservices=( "chargen-stream" "daytime-dgram" "daytime-stream" "echo-dgram" "echo-stream" "tcpmux-server" ) 
+
+for eachchkservice in ${chkservices[*]}
+do 
+	checkxinetd=`yum list xinetd | grep "Available Packages"`
+	if [ -n "$checkxinetd" ]
+	then
+		echo "$count. Xinetd is not installed, hence $eachchkservice is not installed"
+		((count++))
+	else
+		checkchkservices=`chkconfig --list $eachchkservice | grep "off"`
+		if [ -n "$checkchkservices" ]
+		then 
+			echo "$count. $eachchkservice - PASSED ($eachchkservice is not active) "
+			((count++))
+		else 
+			echo "$count. $eachchkservice - FAILED ($eachchkservice is active)"
+			((count++))
+		fi
+	fi
+done
+
+printf "\n"
+printf "Special Purpose Services\n"
+
+# 3.1 Set daemon umask
+checkumask=`grep ^umask /etc/sysconfig/init`
+
+if [ "$checkumask" == "umask 027" ]
+then 
+	echo "1. Umask - PASSED (umask is set to 027)"
+else 
+	echo "1. Umask - FAILED (umask is not set to 027)"
+fi
+
+# 3.2 Remove the x window system
+checkxsystem=`ls -l /etc/systemd/system/default.target | grep graphical.target` #Must return empty
+checkxsysteminstalled=`rpm  -q xorg-x11-server-common`	#Must return something
+	
+if [ -z "$checkxsystem" -a -z "$checkxsysteminstalled" ]
+then 
+	echo "2. X Window System - FAILED (Xorg-x11-server-common is installed)"
+elif [ -z "$checkxsystem" -a -n "$checkxsysteminstalled" ]
+then
+	echo "2. X Window System - PASSED (Xorg-x11-server-common is not installed and is not the default graphical interface)"
+elif [ -n "$checkxsystem" -a -z "$checkxsysteminstalled" ]
+then
+	echo "2. X Window System - FAILED (Xorg-x11-server-common is not installed and is the default graphical interface)"
+else 
+	echo "2. X Window System - FAILED (Xorg-x11-server-common is installed and is the default graphical interface)"
+fi
+count=3
+	checkavahi=`systemctl status avahi-daemon | grep inactive` # 3.3 Disable avahi server
+	checkavahi1=`systemctl status avahi-daemon | grep disabled`
+	if [ -n "$checkavahi" -a -n "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - PASSED (Avahi-daemon is inactive and disabled) "
+		((count++))
+	elif [ -n "$checkavahi" -a -z "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is inactive but not disabled)"
+		((count++))
+	elif [ -z "$checkavahi" -a -n "$checkavahi1" ]
+	then 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is disabled but active)"
+		((count++))
+	else 
+		echo "$count. Avahi-daemon - FAILED (Avahi-daemon is active and enabled)"
+		((count++))
+	fi
+	
+	# 3.4 Disable print server - cups
+	checkcups=`systemctl status cups | grep inactive`
+	checkcups1=`systemctl status cups | grep disabled`
+	
+if [ -n "$checkcups" -a -n "$checkcups1" ]
+	then 
+		echo "$count. Cups - PASSED (Cups is inactive and disabled) "
+		((count++))
+	elif [ -n "$checkcups" -a -z "$checkcups1" ]
+	then 
+		echo "$count. Cups - FAILED (Cups is inactive but not disabled)"
+		((count++))
+	elif [ -z "$checkcups" -a -n "$checkcups1" ]
+	then 
+		echo "$count. Cups - FAILED (Cups is disabled but active)"
+		((count++))
+	else 
+		echo "$count. Cups - FAILED (Cups is active and enabled)"
+		((count++))
+	fi
+
+# 3.5 Remove DHCP server
+checkyumdhcp=`yum list dhcp | grep "Available Packages" `
+checkyumdhcpactive=`systemctl status dhcp | grep inactive `
+checkyumdhcpenable=`systemctl status dhcp | grep disabled `
+if [ -n "$checkyumdhcp" ]
+then 
+	echo "$count. DHCP Server - PASSED (DHCP is not installed) "
+	((count++))
+else 
+	if [ -z "$checkyumdhcpactive" -a -z "$checkyumdhcpenable" ]
+	then 
+		echo "$count. DHCP - FAILED (DHCP is active and enabled)"
+		((count++))
+	elif [ -z "$checkyumdhcpactive" -a -n "$checkyumdhcpenable" ]
+	then 
+		echo "$count. DHCP - FAILED (DHCP is active but disabled)"
+		((count++))
+	elif [ -n "$checkyumdhcpactive" -a -z "$checkyumdhcpenable" ]
+	then
+		echo "$count. DHCP - FAILED (DHCP is inactive but enabled)"
+		((count++))
+	else 
+		echo "$count. DHCP - FAILED (DHCP is inactive but disabled)"
+		((count++))
+	fi
 fi
 
 # 3.6 Configure NTP
@@ -737,4 +1036,17 @@ then
 else
 	echo "$count. Modify the System's Mandatory Access Controls Events - PASSED (Recording of modified system's mandatory access controls events is configured)"
 	((count++))
+fi
+
+# 6.2.1.10 Collect login and logout events
+chklogins=`grep logins /etc/audit/audit.rules`
+loginfail=`grep "\-w /var/log/faillog -p wa -k logins" /etc/audit/audit.rules`
+loginlast=`grep "\-w /var/log/lastlog -p wa -k logins" /etc/audit/audit.rules`
+logintally=`grep "\-w /var/log/tallylog -p wa -k logins" /etc/audit/audit.rules`
+
+if [ -z "$loginfail" -o -z "$loginlast" -o -z "$logintally" ]
+then
+        echo "FAILED - Login and logout events not recorded."
+else
+        echo "PASSED - Login and logout events recorded."
 fi
